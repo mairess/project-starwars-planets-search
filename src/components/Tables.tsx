172b9692a8de03f5d6a3bc259c022721e6c382d6
Maires Souza <trabalho.maires@gmail.com>
2023-10-12 10:02:03 -0300

    add: inputs

--- src/components/Tables.tsx
+++ src/components/Tables.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import PlanetsConstext from '../context/PlanetsContext';
 import { columns } from '../helpers/tableColumns';
 
-const TableStyled = styled.table`
+const TableStyled = styled.div`
 table {
   border-collapse: collapse;
 }
@@ -25,46 +25,76 @@ function Table() {
   });
 
   return (
-    <TableStyled>
-      <label htmlFor="searchInput">
-        Serach here
-      </label>
-      <input
-        id="searchInput"
-        type="text"
-        data-testid="name-filter"
-        value={ searchTerm }
-        onChange={ (event) => setSearchTerm(event.target.value) }
-      />
-      <table>
-        <thead>
-          <tr>
-            {columns.map((column) => (
-              <th key={ column }>{column}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {filteredPlanets.map((planet) => (
-            <tr key={ planet.name }>
-              <td>{planet.name}</td>
-              <td>{planet.rotation_period}</td>
-              <td>{planet.orbital_period}</td>
-              <td>{planet.diameter}</td>
-              <td>{planet.climate}</td>
-              <td>{planet.gravity}</td>
-              <td>{planet.terrain}</td>
-              <td>{planet.surface_water}</td>
-              <td>{planet.population}</td>
-              <td>{planet.films}</td>
-              <td>{planet.created}</td>
-              <td>{planet.edited}</td>
-              <td>{planet.url}</td>
+    <>
+      <div>
+        <label htmlFor="searchInput">
+          Serach here
+        </label>
+        <input
+          id="searchInput"
+          type="text"
+          data-testid="name-filter"
+          value={ searchTerm }
+          onChange={ (event) => setSearchTerm(event.target.value) }
+        />
+      </div>
+      <select
+        data-testid="column-filter"
+        name=""
+        id=""
+      >
+        <option value="population">population</option>
+        <option value="orbital_period">orbital_period</option>
+        <option value="diameter">diameter</option>
+        <option value="rotation_period">rotation_period</option>
+        <option value="surface_water">surface_water</option>
+      </select>
+
+      <select
+        data-testid="comparison-filter"
+        name=""
+        id=""
+      >
+        <option value="maior que">maior que</option>
+        <option value="menor que">menor que</option>
+        <option value="igual a">igual a</option>
+      </select>
+      <button
+        data-testid="value-filter"
+      >
+        Filtrar
+      </button>
+      <TableStyled>
+        <table>
+          <thead>
+            <tr>
+              {columns.map((column) => (
+                <th key={ column }>{column}</th>
+              ))}
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </TableStyled>
+          </thead>
+          <tbody>
+            {filteredPlanets.map((planet) => (
+              <tr key={ planet.name }>
+                <td>{planet.name}</td>
+                <td>{planet.rotation_period}</td>
+                <td>{planet.orbital_period}</td>
+                <td>{planet.diameter}</td>
+                <td>{planet.climate}</td>
+                <td>{planet.gravity}</td>
+                <td>{planet.terrain}</td>
+                <td>{planet.surface_water}</td>
+                <td>{planet.population}</td>
+                <td>{planet.films}</td>
+                <td>{planet.created}</td>
+                <td>{planet.edited}</td>
+                <td>{planet.url}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </TableStyled>
+    </>
   );
 }
 
