eb608fdcb748af0a40847979303c5e276c700fb7
Maires Souza <trabalho.maires@gmail.com>
2023-10-17 06:02:36 -0300

    feat: add state to sort

--- src/components/SortFilter.tsx
+++ src/components/SortFilter.tsx
@@ -1,11 +1,30 @@
+import { useState } from 'react';
 import { columnsSelect } from '../helpers/tableColumns';
 
 function SortFilter() {
+  const [sortOrder, setSortOrder] = useState({
+    order: {
+      column: 'population',
+      sort: 'ASC',
+    },
+  });
+
   return (
     <div>
       <div>
         <label htmlFor="column-sort">Ordenar</label>
-        <select name="column-sort" data-testid="column-sort">
+        <select
+          name="column-sort"
+          data-testid="column-sort"
+          onChange={ (event) => {
+            setSortOrder({
+              order: {
+                column: event.target.value,
+                sort: sortOrder.order.sort,
+              },
+            });
+          } }
+        >
           {columnsSelect.map((column) => (
             <option key={ column } value={ column }>{column}</option>
           ))}
@@ -19,6 +38,14 @@ function SortFilter() {
           name="sort"
           value="ASC"
           id="sort-asc"
+          onChange={ () => {
+            setSortOrder({
+              order: {
+                column: sortOrder.order.column,
+                sort: 'ASC',
+              },
+            });
+          } }
         />
       </div>
       <div>
@@ -29,6 +56,14 @@ function SortFilter() {
           name="sort"
           value="DESC"
           id="sort-desc"
+          onChange={ () => {
+            setSortOrder({
+              order: {
+                column: sortOrder.order.column,
+                sort: 'DESC',
+              },
+            });
+          } }
         />
       </div>
 
